534f2f674ed4ddcd000f8e2c7386a42ac6a0165e
xiaogouxiong <wzl_2@hotmail.com>
2016-11-02 16:15:10 +0800

    stockItem and warehouseDistrict

--- src/database/UpdateTables.java
+++ src/database/UpdateTables.java
@@ -1,85 +1,110 @@
 package database;
 
-import org.bson.Document;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 public class UpdateTables {
+	private MongoClient mongoClient;
+	private DB db;
 
-	public static void main(String[] args) {
-		  try{
-		         // To connect to mongodb server
-		        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-		         // Now connect to your databases
-				MongoDatabase db = mongoClient.getDatabase( "team3" );
-		        
-				//find all district
-		        MongoCollection<Document> districtTable = db.getCollection("district");
-		        MongoCollection<Document> warehouseTable = db.getCollection("warehouse");       
-		        
-		        MongoCursor<Document> cursor = districtTable.find().iterator();
-		         while (cursor.hasNext()) {
-		        	
-		        	Document districtObject = cursor.next();
-		        	System.out.println(districtObject.getString("d_w_id"));
-		        	
-		        	BasicDBObject district = new BasicDBObject();
-		        	district.put("d_id", districtObject.getInteger("d_id"));
-		        	district.put("d_name", districtObject.getString("d_name"));
-		        	district.put("d_street_1", districtObject.getString("d_street_1"));
-		        	district.put("d_street_2", districtObject.getString("d_street_2"));
-		        	district.put("d_city", districtObject.getString("d_city"));
-	        		district.put("d_state", districtObject.getString("d_state"));
-	        		district.put("d_zip", districtObject.getInteger("d_zip"));
-	        		district.put("d_tax", districtObject.getDouble("d_tax"));
-	        		district.put("d_ytd", districtObject.getDouble("d_ytd"));
-	        		district.put("d_next_o_id", districtObject.getInteger("d_next_o_id"));
-		        	  
-		        	//find the ware house record
-		        	BasicDBObject searchWarehouse = new BasicDBObject();
-		        	searchWarehouse.put("w_id", districtObject.getInteger("d_w_id"));
-		        	
-		        	BasicDBObject updateObj = new BasicDBObject();
-		        	updateObj.put("$push", new BasicDBObject("district",district));
-		        	
-//		        	BasicDBObject updateObj = new BasicDBObject();
-//		        	updateObj.put("district", insertData);
-		        	warehouseTable.updateOne(searchWarehouse, updateObj);
-		        	
-//		        	DBCursor cursor2 = warehouseTable.find(searchWarehouse);
-//		        	while (cursor2.hasNext()) {
-//		        		BasicDBObject warehouseObject = (BasicDBObject) cursor.next();
-//		        		warehouseDistrictObject.put("w_id", warehouseObject.getInt("w_id"));
-//		        		warehouseDistrictObject.put("w_name", warehouseObject.getString("w_name"));
-//		        		warehouseDistrictObject.put("w_street_1", warehouseObject.getString("w_street_1"));
-//		        		warehouseDistrictObject.put("w_street_2", warehouseObject.getString("w_street_2"));
-//		        		warehouseDistrictObject.put("w_city", warehouseObject.getString("w_city"));
-//		        		warehouseDistrictObject.put("w_state", warehouseObject.getString("w_state"));
-//		        		warehouseDistrictObject.put("w_zip", warehouseObject.getInt("w_zip"));
-//		        		warehouseDistrictObject.put("w_tax", warehouseObject.getDouble("w_tax"));
-//		        		warehouseDistrictObject.put("w_ytd", warehouseObject.getDouble("w_ytd"));
-//		        		
-//		        		warehouseObject.put("district", district);
-//		        		
-//		        	}
-		        	
-		         }
-		         
-		         System.out.println("Connect to database successfully");
-		         
-		      }catch(Exception e){
-		    	  
-		         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		         
-		      }
-
+	
+	public UpdateTables(){
+		// To connect to mongodb server
+		this.mongoClient = new MongoClient( "localhost" , 27017 );
+		
+		// Connect to databases
+		this.db = mongoClient.getDB( "team3" );
+		
 	}
-
+	
+	public static void main(String[] args) {
+		UpdateTables upData = new UpdateTables();
+		upData.updateWareHouse();
+		upData.updateStockItem();
+		
+        System.out.println("Update successfully");
+	}
+	
+	public void updateWareHouse(){
+		//create the index
+		db.getCollection("warehouse").createIndex(new BasicDBObject("w_id",1));
+		
+		//find all district
+        DBCollection districtTable = db.getCollection("district");
+        DBCollection warehouseTable = db.getCollection("warehouse");
+        DBCursor cursor = districtTable.find();
+        
+        while (cursor.hasNext()) {
+        	BasicDBObject districtObject = (BasicDBObject) cursor.next();
+        	System.out.println(districtObject.getString("d_w_id"));
+        	
+        	BasicDBObject district = new BasicDBObject();
+        	district.put("d_id", districtObject.getInt("d_id"));
+        	district.put("d_name", districtObject.getString("d_name"));
+        	district.put("d_street_1", districtObject.getString("d_street_1"));
+        	district.put("d_street_2", districtObject.getString("d_street_2"));
+        	district.put("d_city", districtObject.getString("d_city"));
+    		district.put("d_state", districtObject.getString("d_state"));
+    		district.put("d_zip", districtObject.getInt("d_zip"));
+    		district.put("d_tax", districtObject.getDouble("d_tax"));
+    		district.put("d_ytd", districtObject.getDouble("d_ytd"));
+    		district.put("d_next_o_id", districtObject.getInt("d_next_o_id"));
+        	  
+        	//find the ware house record
+        	BasicDBObject searchWarehouse = new BasicDBObject();
+        	searchWarehouse.put("w_id", districtObject.getInt("d_w_id"));
+        	
+        	BasicDBObject updateObj = new BasicDBObject();
+//        	updateObj.put("$push", new BasicDBObject("district",district));
+        	updateObj.put("$set", new BasicDBObject(districtObject.getString("d_id"),district));
+        	
+        	
+        	warehouseTable.update(searchWarehouse, updateObj);
+         }
+        warehouseTable.rename("warehouseDistrict");
+	}
+	
+	public void updateStockItem() {
+		//create the index
+		db.getCollection("stock").createIndex(new BasicDBObject("s_i_id",1), new BasicDBObject("s_w_id",1));
+		
+		//find all district
+        DBCollection stockTable = db.getCollection("stock");
+        DBCollection itemTable = db.getCollection("item");
+        
+        DBCursor cursor = itemTable.find();
+        //disable timeout
+        cursor.addOption(com.mongodb.Bytes.QUERYOPTION_NOTIMEOUT);
+        
+        while (cursor.hasNext()) {
+        	BasicDBObject itemObject = (BasicDBObject) cursor.next();
+        	System.out.println(itemObject.getString("i_id"));
+        	
+        	BasicDBObject item = new BasicDBObject();
+        	item.append("i_name", itemObject.getString("i_name"));
+        	item.append("i_price", itemObject.getDouble("i_price"));
+        	item.append("i_im_id", itemObject.getInt("i_im_id"));
+        	item.append("i_data", itemObject.getString("i_data"));
+//        	item.put("$set", new BasicDBObject().append("i_name", itemObject.getString("i_name")));
+//        	item.put("$set", new BasicDBObject().append("i_price", itemObject.getDouble("i_price")));
+//        	item.put("$set", new BasicDBObject().append("i_im_id", itemObject.getInt("i_im_id")));
+//        	item.put("$set", new BasicDBObject().append("i_data", itemObject.getString("i_data")));
+        	
+        	
+        	//find the ware house record
+        	BasicDBObject searchStock = new BasicDBObject();
+        	searchStock.append("s_i_id", itemObject.getInt("i_id"));
+        	
+        	BasicDBObject updateObj = new BasicDBObject();
+        	updateObj.append("$set", item);
+     
+        	stockTable.updateMulti(searchStock, updateObj);
+         }
+        stockTable.rename("sotckItem");
+	}
+	
 }
