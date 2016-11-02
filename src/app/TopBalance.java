e4f3608d488bbb02d75107b65ad7023adfa32a35
bobby-lin <bobby.linjh@gmail.com>
2016-11-02 12:08:37 +0800

    Added BufferedWriter for I/O in TopBalance.

--- src/app/TopBalance.java
+++ src/app/TopBalance.java
@@ -5,6 +5,7 @@ import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Sorts.*;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,40 +37,43 @@ public class TopBalance {
 		this.tableWarehouse = database.getCollection(TABLE_WAREHOUSE);
 	}
 
-	public void processTopBalance(){
+	public void processTopBalance() throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String c_name, warehouseDistrictName;
 		Double c_balance;
-		String output = "";
-		
+
+		bw.write("======Processing TopBalance transaction========\n");
 		ArrayList<Document> customers = selectTopBalance();
 		for(int i = 0; i< customers.size(); i++) {
 			Document customer = customers.get(i);
 			c_name = customer.getString("c_first") + " " + customer.getString("c_middle") + " " + customer.getString("c_last");
 			c_balance = customer.getDouble("c_balance");
 			warehouseDistrictName = getWarehouseDistrictName(customer.getInteger("c_w_id"), customer.getInteger("c_d_id"));
-		    output = output + String.format(MESSAGE_OUTPUT, c_name, c_balance, warehouseDistrictName);
+			bw.write(String.format(MESSAGE_OUTPUT, c_name, c_balance, warehouseDistrictName));
 		}
-		
-		System.out.println(output);
+
+		bw.write("\n");
+		bw.flush();
+
 	}
 
 	private String getWarehouseDistrictName(int c_w_id, int c_d_id) {
 		String name = "";
-		
+
 		List<Bson> pipeline = new ArrayList<Bson>();
 		pipeline.add(match(eq("w_id", c_w_id)));
 		pipeline.add(project(include("w_name","district")));
 		pipeline.add(unwind("$district"));
 		pipeline.add(match(eq("district.d_id", c_d_id)));
 		pipeline.add(project(include("w_name", "district.d_name")));
-		
+
 		MongoCursor<Document> cursor = tableWarehouse.aggregate(pipeline).iterator();
 		while(cursor.hasNext()) {
 			Document doc = cursor.next();
 			Document district = (Document) doc.get("district");
 			name = doc.getString("w_name") + ", " + district.getString("d_name");
 		}
-		
+
 		return name;
 	}
 
@@ -78,10 +82,10 @@ public class TopBalance {
 		pipeline.add(project(fields(include("c_first", "c_middle", "c_last", "c_balance", "c_w_id", "c_d_id"), excludeId())));
 		pipeline.add(sort(descending("c_balance")));
 		pipeline.add(limit(10));
-		
+
 		ArrayList<Document> topTen = tableCustomer.aggregate(pipeline).into(new ArrayList<Document>());
 
 		return topTen;
 	}
-	
+
 }
