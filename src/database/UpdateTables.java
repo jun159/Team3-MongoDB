bcd5234e6ae8b4e898475889e76dd8ad3a3c2b79
xiaogouxiong <wzl_2@hotmail.com>
2016-11-02 16:52:16 +0800

    CreateIndex

--- src/database/UpdateTables.java
+++ src/database/UpdateTables.java
@@ -25,8 +25,14 @@ public class UpdateTables {
 		UpdateTables upData = new UpdateTables();
 		upData.updateWareHouse();
 		upData.updateStockItem();
+		upData.createCustomerIndex();
+		System.out.println("Create customer index successfully");
+		upData.createOrderIndex();
+		System.out.println("Create order index successfully");
+		upData.createOrderLineIndex();
+		System.out.println("Create orderLine index successfully");
 		
-        System.out.println("Update successfully");
+        System.out.println("End of the function");
 	}
 	
 	public void updateWareHouse(){
@@ -70,7 +76,9 @@ public class UpdateTables {
 	
 	public void updateStockItem() {
 		//create the index
-		db.getCollection("stock").createIndex(new BasicDBObject("s_i_id",1), new BasicDBObject("s_w_id",1));
+		db.getCollection("stock").createIndex(new BasicDBObject("s_i_id",1));
+		db.getCollection("stock").createIndex(new BasicDBObject("s_w_id",1));
+		
 		
 		//find all district
         DBCollection stockTable = db.getCollection("stock");
@@ -105,6 +113,61 @@ public class UpdateTables {
         	stockTable.updateMulti(searchStock, updateObj);
          }
         stockTable.rename("stockItem");
+	}
+	
+	public void createCustomerIndex(){
+		db.getCollection("customer").createIndex(new BasicDBObject("c_w_id",1));
+		db.getCollection("customer").createIndex(new BasicDBObject("c_d_id",1));
+		db.getCollection("customer").createIndex(new BasicDBObject("c_id",1));
+	}
+	public void createOrderIndex(){
+		db.getCollection("orders").createIndex(new BasicDBObject("o_w_id",1));
+		db.getCollection("orders").createIndex(new BasicDBObject("o_d_id",1));
+		db.getCollection("orders").createIndex(new BasicDBObject("o_id",1));
+	}
+	public void createOrderLineIndex(){
+		db.getCollection("orderline").createIndex(new BasicDBObject("ol_w_id",1));
+		db.getCollection("orderline").createIndex(new BasicDBObject("ol_d_id",1));
+		db.getCollection("orderline").createIndex(new BasicDBObject("ol_o_id",1));
+	}
+	
+	public void updateOrderOrderLine(){
+		//create the index
+				db.getCollection("orders").createIndex(new BasicDBObject("o_w_id",1));
+				
+				//find all
+		        DBCollection orderTable = db.getCollection("orders");
+		        DBCollection orderLineTable = db.getCollection("orderline");
+		        DBCursor cursor = orderLineTable.find();
+		        
+		        while (cursor.hasNext()) {
+		        	BasicDBObject districtObject = (BasicDBObject) cursor.next();
+		        	System.out.println(districtObject.getString("d_w_id"));
+		        	
+		        	BasicDBObject district = new BasicDBObject();
+		        	district.put("d_id", districtObject.getInt("d_id"));
+		        	district.put("d_name", districtObject.getString("d_name"));
+		        	district.put("d_street_1", districtObject.getString("d_street_1"));
+		        	district.put("d_street_2", districtObject.getString("d_street_2"));
+		        	district.put("d_city", districtObject.getString("d_city"));
+		    		district.put("d_state", districtObject.getString("d_state"));
+		    		district.put("d_zip", districtObject.getInt("d_zip"));
+		    		district.put("d_tax", districtObject.getDouble("d_tax"));
+		    		district.put("d_ytd", districtObject.getDouble("d_ytd"));
+		    		district.put("d_next_o_id", districtObject.getInt("d_next_o_id"));
+		        	  
+		        	//find the ware house record
+		        	BasicDBObject searchWarehouse = new BasicDBObject();
+		        	searchWarehouse.put("w_id", districtObject.getInt("d_w_id"));
+		        	
+		        	BasicDBObject updateObj = new BasicDBObject();
+//		        	updateObj.put("$push", new BasicDBObject("district",district));
+		        	updateObj.put("$set", new BasicDBObject(districtObject.getString("d_id"),district));
+		        	
+		        	
+		        	orderTable.update(searchWarehouse, updateObj);
+		         }
+		        orderTable.rename("xxx");
 	}
 	
 }
